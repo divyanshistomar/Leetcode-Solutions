af42356c13d0d688276a6fe238b01b54dca33b66
Divyanshi Singh Tomar <divyanshisinghtomar05@gmail.com>
2026-09-23 12:27:44 +0530

    Time: 15 ms (59.73%), Space: 51.3 MB (87.21%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int n = g.length;
+        int m = s.length;
+
+        int l = 0;
+        int r = 0;
+
+        while (l < m && r < n) {
+            if (g[r] <= s[l]) {
+                r++;
+                l++;
+            } else {
+                l++;
+            }
+        }
+
+        return r;
+    }
+}
